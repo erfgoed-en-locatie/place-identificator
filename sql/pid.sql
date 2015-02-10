49220356b29aac81ecf5fa0dc22edc3b7b5b2db9
PHPetra <phpetra@xs4all.nl>
2015-02-10 12:34:49 +0100

    new sql file with simple users and some data

---
+++ sql/pid.sql
@@ -0,0 +1,151 @@
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `pid`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `datasets`
+--
+
+CREATE TABLE IF NOT EXISTS `datasets` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(127) DEFAULT NULL,
+  `filename` varchar(255) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `status` int(11) unsigned DEFAULT NULL,
+  `created_on` datetime DEFAULT NULL,
+  `updated_on` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `dataset_user` (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
+
+--
+-- Dumping data for table `datasets`
+--
+
+INSERT INTO `datasets` (`id`, `name`, `filename`, `user_id`, `status`, `created_on`, `updated_on`) VALUES
+  (4, 'Eerste set', 'fake-file.csv', 1, 2, '2015-02-09 00:00:00', NULL),
+  (5, 'Tweede set', 'weer-fake.csv', NULL, 3, '2015-02-09 10:22:24', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `multiples`
+--
+
+CREATE TABLE IF NOT EXISTS `multiples` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `record_id` int(11) DEFAULT NULL,
+  `geonames` text,
+  `tgn` text,
+  `bag` text,
+  `gg` text,
+  `erfgeo` text,
+  `created_on` datetime NOT NULL,
+  `updated_on` datetime DEFAULT NULL,
+  `status` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `multiple_record` (`record_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `records`
+--
+
+CREATE TABLE IF NOT EXISTS `records` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `dataset_id` int(11) DEFAULT NULL,
+  `original_name` varchar(255) NOT NULL,
+  `geonames` text,
+  `tgn` text,
+  `bag` text,
+  `gg` text,
+  `erfgeo` text,
+  `created_on` datetime NOT NULL,
+  `updated_on` datetime DEFAULT NULL,
+  `status` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `record_dataset` (`dataset_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(100) NOT NULL DEFAULT '',
+  `password` varchar(255) DEFAULT NULL,
+  `salt` varchar(255) NOT NULL DEFAULT '',
+  `roles` varchar(255) NOT NULL DEFAULT '',
+  `name` varchar(100) NOT NULL DEFAULT '',
+  `time_created` int(11) unsigned NOT NULL DEFAULT '0',
+  `username` varchar(100) DEFAULT NULL,
+  `isEnabled` tinyint(1) NOT NULL DEFAULT '1',
+  `confirmationToken` varchar(100) DEFAULT NULL,
+  `timePasswordResetRequested` int(11) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_email` (`email`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `password`, `salt`, `roles`, `name`, `time_created`, `username`, `isEnabled`, `confirmationToken`, `timePasswordResetRequested`) VALUES
+  (1, 'dreis@xs4all.nl', 'BWnyqbUBzfFRCty5zlJvBeJjv3onihqjxQqAd744OmDPxQyhdg3xyqMb2ln4D8O2WLOaDNdoSqG1ofVwA+PqoA==', 'f4xn693cvpk4ogo08gw8s8sk0sc4ksg', 'ROLE_USER,ROLE_ADMIN', 'Petra', 1423521374, NULL, 1, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_custom_fields`
+--
+
+CREATE TABLE IF NOT EXISTS `user_custom_fields` (
+  `user_id` int(11) NOT NULL,
+  `attribute` varchar(50) NOT NULL DEFAULT '',
+  `value` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`user_id`,`attribute`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `datasets`
+--
+ALTER TABLE `datasets`
+ADD CONSTRAINT `dataset_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE SET NULL;
+
+--
+-- Constraints for table `multiples`
+--
+ALTER TABLE `multiples`
+ADD CONSTRAINT `multiple_ibfk_2` FOREIGN KEY (`record_id`) REFERENCES `records` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `records`
+--
+ALTER TABLE `records`
+ADD CONSTRAINT `record_ibfk_1` FOREIGN KEY (`dataset_id`) REFERENCES `datasets` (`id`) ON DELETE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
