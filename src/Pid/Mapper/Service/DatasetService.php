882f526923e0ae1f490123f6f4dc21e7fd5b9822
PHPetra <phpetra@xs4all.nl>
2015-02-18 13:41:44 +0100

    Storing changes

--- src/Pid/Mapper/Service/DatasetService.php
+++ src/Pid/Mapper/Service/DatasetService.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Pid\Mapper\Service;
+use Pid\Mapper\Model\Status;
 
 
 /**
@@ -35,11 +36,30 @@ class DatasetService {
                 'id' => (int)$id
             ));
         } else {
-            $stmt = $this->db->executeQuery('SELECT * FROM datasets WHERE id = :id AND user_id = :userId', array(
+            $sql = 'SELECT * FROM datasets WHERE id = :id AND user_id = :userId';
+            $params = array(
                 'id' => (int)$id,
-                'userId' => (int)$userId
-            ));
+                'userId' => (int)$userId);
+            $stmt = $this->db->executeQuery($sql, $params);
         }
+        return $stmt->fetch();
+    }
+
+    // todo finish this query
+    public function fetchCountForDatasetWithStatus($id, $status)
+    {
+        $sql = '
+          SELECT d.* COUNT(r.*)
+          FROM records r
+          JOIN datasets d ON d.id = r.dataset_id
+          WHERE d.id = :id
+          AND r.status = :status
+          ';
+        $params = array(
+            'id' => (int) $id,
+            'status' => $status
+        );
+        $stmt = $this->db->executeQuery($sql, $params);
         return $stmt->fetch();
     }
 
@@ -57,6 +77,7 @@ class DatasetService {
         return $this->db->insert('field_mapping', $data);
     }
 
+
     public function getPlaceColumnForDataset($id)
     {
         $stmt = $this->db->executeQuery('SELECT placename FROM field_mapping WHERE dataset_id = :id', array(
@@ -64,5 +85,20 @@ class DatasetService {
         ));
         return $stmt->fetchColumn(0);
     }
+
+    /**
+     * Save the provided mapping
+     *
+     * @param array $data
+     * @return int
+     */
+    public function storeMappedRecords($data)
+    {
+        $date = new \DateTime('now');
+        $data['created_on'] = $date->format('Y-m-d H:i:s');
+
+        return $this->db->insert('records', $data);
+    }
+
 
 }
