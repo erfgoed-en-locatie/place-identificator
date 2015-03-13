5e80c7a3a4b0a8897d126102be6bfae7655ddb26
PHPetra <phpetra@xs4all.nl>
2015-03-13 23:02:46 +0100

    moving configs to bootstrap

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -6,7 +6,9 @@
 
 // CUSTOM services
 use Monolog\Logger;
+use Knp\Provider\ConsoleServiceProvider;
 use Symfony\Component\HttpFoundation\Request;
+
 
 $app['dataset_service'] = $app->share(function ($app) {
     return new \Pid\Mapper\Service\DatasetService($app['db']);
@@ -18,6 +20,100 @@ $app['uri_resolver_service'] = $app->share(function ($app) {
     return new \Pid\Mapper\Service\UriResolverService();
 });
 
+
+// CONSOLE
+$app->register(new ConsoleServiceProvider(), array(
+    'console.name' => 'ConsoleApp',
+    'console.version' => '1.0.0',
+    'console.project_directory' => __DIR__ . '/..'
+));
+
+// SIMPLE USER
+$app['user.options'] = array(
+
+    // Specify custom view templates here.
+    'templates' => array(
+        'layout' => 'layout.html.twig',
+        'register' => 'simple-user/register.twig',
+        'register-confirmation-sent' => 'simple-user/register-confirmation-sent.twig',
+        'login' => 'simple-user/login.twig',
+        'login-confirmation-needed' => 'simple-user/login-confirmation-needed.twig',
+        'forgot-password' => 'simple-user/forgot-password.twig',
+        'reset-password' => 'simple-user/reset-password.twig',
+        'view' => 'simple-user/view.twig',
+        'edit' => 'simple-user/edit.twig',
+        'list' => 'simple-user/list.twig',
+        //'list' => '@user/list.twig',
+    ),
+
+
+    // Configure the user mailer for sending password reset and email confirmation messages.
+    'mailer' => array(
+        'enabled' => true, // When false, email notifications are not sent (they're silently discarded).
+        'fromEmail' => array(
+            'address' => 'do-not-reply@' . (isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : gethostname()),
+            'name' => null,
+        ),
+    ),
+
+    'emailConfirmation' => array(
+        'required' => false, // Whether to require email confirmation before enabling new accounts.
+        'template' => '@user/email/confirm-email.twig',
+    ),
+
+    'passwordReset' => array(
+        'template' => '@user/email/reset-password.twig',
+        'tokenTTL' => 86400, // How many seconds the reset token is valid for. Default: 1 day.
+    ),
+
+    // Set this to use a custom User class.
+    'userClass' => 'SimpleUser\User',
+
+    // Whether to require that users have a username (default: false).
+    // By default, users sign in with their email address instead.
+    'isUsernameRequired' => false,
+
+    // A list of custom fields to support in the edit controller.
+    'editCustomFields' => array(),
+
+    // Override table names, if necessary.
+    'userTableName' => 'users',
+    'userCustomFieldsTableName' => 'user_custom_fields',
+);
+
+// Security config, for SIMPLE USER See http://silex.sensiolabs.org/doc/providers/security.html for details.
+$app['security.firewalls'] = array(
+    // Ensure that the login page is accessible to all, if you set anonymous => false below.
+    'login' => array(
+        'pattern' => '^/user/login$',
+    ),
+    'register' => array(
+        'pattern' => '^/user/register$',
+    ),
+    'forgot_password' => array(
+        'pattern' => '^/user/forgot-password$',
+    ),
+    'homepage' => array(
+        'pattern' => '^/$',
+    ),
+    'colofon' => array(
+        'pattern' => '^/colofon$',
+    ),
+    // everything else is secured
+    'secured_area' => array(
+        'pattern' => '^.*$',
+        'anonymous' => false,
+        'remember_me' => array(),
+        'form' => array(
+            'login_path' => '/user/login',
+            'check_path' => '/user/login_check',
+        ),
+        'logout' => array(
+            'logout_path' => '/user/logout',
+        ),
+        'users' => $app->share(function($app) { return $app['user.manager']; }),
+    ),
+);
 
 // TWIG
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
